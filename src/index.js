4ada7d38d5b5c6533bae4b04389afe9a934b62a5
Worldsalt3 <riliwanhazzan@gmail.com>
2021-01-09 23:22:29 +0100

    fetch api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const url = 'http://api.enye.tech/v1/challenge/records';
+
+const Record = () => {
+  const [record, setRecord] = useState([]);
+  
+ 
+
+  const getProfiles = async () => {
+    const response = await fetch(url);
+    const record = await response.json();
+    setRecord(record.records.profiles)
+  }
+
+  useEffect(async () => {
+    getProfiles();
+  }, [])
+  return (
+    <>
+      <h1>Hello World</h1>
+      {record.map((profile, index) => {
+        const {FirstName} = profile;
+        return <p>{FirstName}</p>
+      })}
+    </>
+  )
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Record />
+  ,
   document.getElementById('root')
 );
 
